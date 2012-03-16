bdc3fcda7c237bdda1cebe9f29867cb69e4e901b
Matias Eidis <matiaseidis@gmail.com>
2012-03-16 11:52:54 -0300

    stuff

---
+++ src/main/groovy/TestController.groovy
@@ -0,0 +1,20 @@
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.mati.demo.prevalence.BaseModel;
+
+@Controller
+class TestController {
+	
+	BaseModel baseModel
+	
+	@RequestMapping("/test")
+	def @ResponseBody String profile(ModelAndView m) {
+		"Hello"
+	}
+
+
+}
--- src/main/groovy/com/mati/demo/controller/TestController.groovy
+++ src/main/groovy/com/mati/demo/controller/TestController.groovy
@@ -2,19 +2,18 @@ package com.mati.demo.controller
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.servlet.ModelAndView
-import groovy.lang.GroovyClassLoader
 import com.mati.demo.prevalence.BaseModel
 
 @Controller
-class ProfileController {
+class TestController {
 	
 	BaseModel baseModel
 	
-	@RequestMapping("/profile")
-	def profile(ModelAndView m) {
-		def user = baseModel.getModel().getLoggedInUser()
-		m.addObject("user", user)
+	@RequestMapping("/test")
+	def ModelAndView profile(ModelAndView m) {
+		m
 	}
 
 
